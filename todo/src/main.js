97f964ce546ebae0077042420faf8f8844b195eb
Sandy <sandy_wheeler@clinicalarchitecture.com>
2021-07-30 16:28:49 -0400

    got vuetify added

--- todo/src/main.js
+++ todo/src/main.js
@@ -1,10 +1,35 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import vuetify from './plugins/vuetify'
 Vue.config.productionTip = false
+
+// new Vue({
+//   router,
+//   render: h => h(App)
+// }).$mount('#app')
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  vuetify,
+  render: (h) => h(App),
+  el: '#app',
+  data: {
+    item: '+ Add New ...',
+    // item: {
+    //   text: '',
+    // },
+    items: [
+      {
+        text: 'first item',
+      },
+    ],
+  },
+  methods: {
+    addNew() {
+      const { text } = this.item;
+      this.items.push({ text });
+      this.item.text = '';
+    },
+  },
+}).$mount('#app');
