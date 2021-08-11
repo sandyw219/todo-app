0354382537c163dba88cc0a38483ca48bddd4f2d
Sandy <sandy_wheeler@clinicalarchitecture.com>
2021-08-11 17:10:02 -0400

    made daily components and passed data through

--- todo/src/components/thisWeekTodo.vue
+++ todo/src/components/thisWeekTodo.vue
@@ -16,84 +16,47 @@
         </h1>
         <v-divider></v-divider>
       </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-        justify="center"
-      >
-        <h2 class="headline font-weight-bold mb-3 text-decoration-underline">
-          Monday
-        </h2>
-        
-        <v-card
-        class="text-left"> 
-
-        <h3> <pre>    Work:</pre></h3>
-          <ul
-            v-for="(item, i) in monday.work"
-            :key="i"
-            class="subheading mx-3"
-            target="_blank"
-          >
-
-          <input type="checkbox"
-          class="subheading mx-3"
-            :id="item.text"
-            >
-            <label v-if="item.complete==true" class="text-decoration-line-through" :for="item.text">{{ item.text }}</label>
-            <label v-else :for="item.text">{{ item.text }}</label>
-            <v-divider class="mx-4" vertical></v-divider>
-            <!-- subList button -->
-            <button class="addNewSmall" v-if="item.sublist" v-on:click="goTo('SubList')">More</button>
-            <button class="addNewSmall" v-else v-on:click="goTo('SubList')">+</button>
-          </ul>
-
-          <h3> <pre>    Personal:</pre></h3>
-          <ul
-            v-for="(item, i) in monday.personal"
-            :key="i"
-            class="subheading mx-3"
-            target="_blank"
-          >
-
-          <input type="checkbox"
-          class="subheading mx-3"
-            :id="item.text"
-            :value="item.complete"
-            @change="check($event, item)"
-            >
-            <label :for="item.text">{{ item.text }}</label>
-            <v-divider class="mx-4" vertical></v-divider>
-            <!-- subList button -->
-            <button class="addNewSmall" v-if="item.sublist" v-on:click="goTo('SubList')">More</button>
-            <button class="addNewSmall" v-else v-on:click="goTo('SubList')">+</button>
-          </ul>
-
-        </v-card>
-        <!-- groceries button -->
-        <v-btn
-          to="/subList"
-          text
-          >
-          <span
-            style="color: #66BB6A;"
-            >
-            grocery list
-          </span>
-        </v-btn>
-
-        <button v-on:click="goTo()">G list</button>
-
+      <!-- days of the week -->
+      <v-col cols="12">
+      <daily-todo-component :day="week.mon"/>
       </v-col>
+      <v-col cols="12">
+      <daily-todo-component :day="week.tue"/>
+      </v-col>
+      <v-col cols="12">
+      <daily-todo-component :day="week.wed"/>
+      </v-col>
+      <v-col cols="12">
+      <daily-todo-component :day="week.thu"/>
+      </v-col>
+      <v-col cols="12">
+      <daily-todo-component :day="week.fri"/>
+      </v-col>
+      <v-col cols="12">
+      <daily-todo-component :day="week.sat"/>
+      </v-col>
+      <v-col cols="12">
+      <daily-todo-component :day="week.sun"/>
+      </v-col>
+      
     </v-row>
+    
   </v-container>
 </template>
 
 <script>
+import dailyTodoComponent from '../components/dailyTodoComponent.vue';
   // import router from '../router/index.js'
   export default {
+    components: { dailyTodoComponent },
     name: 'todoList',
+
+    props: {
+        week: {
+            type: Object,
+            default: () => ({})
+        }
+    },
 
     methods: {
       goTo: function (path) {
@@ -106,32 +69,7 @@
     },
 
     data: () => ({
-      monday: {
-        work: [ 
-          {
-            text: 'A2',
-            sublist: null,
-            complete: false,
-          },
-          {
-            text: 'C311 Project',
-            sublist: null,
-            complete: true,
-          },
-        ],
-        personal: [
-          {
-            text: 'Groceries',
-            sublist: ['oranges', 'pickles', 'chocolate', 'paper towels'],
-            complete: false,
-          },
-          {
-            text: 'Clip Toenails',
-            sublist: null,
-            complete: false,
-          },
-        ],
-      }
+      //
     }),
   }
 </script>
--- todo/src/views/Home.vue
+++ todo/src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <todo-list />
+  <todo-list :week="thisWeek"/>
 </template>
 
 <script>
@@ -20,5 +20,205 @@
     components: {
       todoList,
     },
+
+    data: () => ({
+      thisWeek: {
+        mon: {
+          title: 'Monday',
+          work: [ 
+            {
+              text: 'A2',
+              sublist: null,
+              complete: false,
+            },
+            {
+              text: 'C311 Project',
+              sublist: null,
+              complete: true,
+            },
+          ],
+          personal: [
+            {
+              text: 'Groceries',
+              sublist: ['oranges', 'pickles', 'chocolate', 'paper towels'],
+              complete: false,
+            },
+            {
+              text: 'Clip Toenails',
+              sublist: null,
+              complete: false,
+            },
+          ],
+        },
+        //
+        tue: {
+          title: 'Tuesday',
+          work: [ 
+            {
+              text: 'A2',
+              sublist: null,
+              complete: false,
+            },
+            {
+              text: 'C311 Project',
+              sublist: null,
+              complete: true,
+            },
+          ],
+          personal: [
+            {
+              text: 'Groceries',
+              sublist: ['oranges', 'pickles', 'chocolate', 'paper towels'],
+              complete: false,
+            },
+            {
+              text: 'Clip Toenails',
+              sublist: null,
+              complete: false,
+            },
+          ],
+        },
+        //
+        wed: {
+          title: 'Wednesday',
+          work: [ 
+            {
+              text: 'A2',
+              sublist: null,
+              complete: false,
+            },
+            {
+              text: 'C311 Project',
+              sublist: null,
+              complete: true,
+            },
+          ],
+          personal: [
+            {
+              text: 'Groceries',
+              sublist: ['oranges', 'pickles', 'chocolate', 'paper towels'],
+              complete: false,
+            },
+            {
+              text: 'Clip Toenails',
+              sublist: null,
+              complete: false,
+            },
+          ],
+        },
+        //
+        thu: {
+          title: 'Thursday',
+          work: [ 
+            {
+              text: 'A2',
+              sublist: null,
+              complete: false,
+            },
+            {
+              text: 'C311 Project',
+              sublist: null,
+              complete: true,
+            },
+          ],
+          personal: [
+            {
+              text: 'Groceries',
+              sublist: ['oranges', 'pickles', 'chocolate', 'paper towels'],
+              complete: false,
+            },
+            {
+              text: 'Clip Toenails',
+              sublist: null,
+              complete: false,
+            },
+          ],
+        },
+        //
+        fri: {
+          title: 'Friday',
+          work: [ 
+            {
+              text: 'A2',
+              sublist: null,
+              complete: false,
+            },
+            {
+              text: 'C311 Project',
+              sublist: null,
+              complete: true,
+            },
+          ],
+          personal: [
+            {
+              text: 'Groceries',
+              sublist: ['oranges', 'pickles', 'chocolate', 'paper towels'],
+              complete: false,
+            },
+            {
+              text: 'Clip Toenails',
+              sublist: null,
+              complete: false,
+            },
+          ],
+        },
+      //
+      sat: {
+          title: 'Saturday',
+          work: [ 
+            {
+              text: 'A2',
+              sublist: null,
+              complete: false,
+            },
+            {
+              text: 'C311 Project',
+              sublist: null,
+              complete: true,
+            },
+          ],
+          personal: [
+            {
+              text: 'Groceries',
+              sublist: ['oranges', 'pickles', 'chocolate', 'paper towels'],
+              complete: false,
+            },
+            {
+              text: 'Clip Toenails',
+              sublist: null,
+              complete: false,
+            },
+          ],
+        },
+        //
+        sun: {
+          title: 'Sunday',
+          work: [ 
+            {
+              text: 'A2',
+              sublist: null,
+              complete: false,
+            },
+            {
+              text: 'C311 Project',
+              sublist: null,
+              complete: true,
+            },
+          ],
+          personal: [
+            {
+              text: 'Groceries',
+              sublist: ['oranges', 'pickles', 'chocolate', 'paper towels'],
+              complete: false,
+            },
+            {
+              text: 'Clip Toenails',
+              sublist: null,
+              complete: false,
+            },
+          ],
+        },
+      },
+    }),
   }
 </script>
